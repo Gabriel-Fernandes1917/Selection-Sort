5b9c16cf32915735ca762ba92b02efbf4324ece8
Gabriel Fernandes <gabgui2001@gmail.com>
2021-08-05 10:32:41 -0300

    finished Selection Sort

--- Selection Sort.cpp
+++ Selection Sort.cpp
@@ -4,7 +4,10 @@
 using namespace std;
 
 int vet[5];
-int changer;
+int Min;
+int Max;            
+int Posi = 0;
+
 
 int main(int argc, char const *argv[])
 {
@@ -19,9 +22,69 @@ int main(int argc, char const *argv[])
 
     for (int i = 0; i < 5; i++)
     {
-        cout << vet[i] << " " ;
+        if (i == 0)
+        {
+            cout << "[ " << vet[i] << ", ";
+        }else
+        if (i == 4)
+        {
+            cout << vet[i] << " ]" ;
+        }else{
+            cout << vet[i] << ", " ;
+        }
+           
     }
+
+    //Selection Sort
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = i+1; j < 5; j++)
+        {
+            if (vet[i] > vet[j])
+            {
+                Max = vet[i];//4
+                
+                Min = vet[j];//1
+                
+
+                cout << "Min = " << Min << endl;
+                vet[i] = Min;
+                vet[j] = Max;
+                
+
+                cout << "Max : " << Max << endl;
+            }
+                
+        }
+        
+
+ 
+    }
+
     
+        
+    
+    // end
+    
+    
+    cout << "your new array is : " << endl;
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (i == 0)
+        {
+            cout << "[ " << vet[i] << ", ";
+        }else
+        if (i == 4)
+        {
+            cout << vet[i] << " ]" ;
+        }else{
+            cout << vet[i] << ", " ;
+        }
+           
+    }
+
     
 
     return 0;
